b03a1538a93b827d10744444f17c2f43fa28ba77
StefM <smouton8993@gmail.com>
2020-07-22 10:07:38 -0500

    Adding assets, set up project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D Controller;
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
